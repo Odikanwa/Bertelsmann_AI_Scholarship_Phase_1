225c936f02bebc9ecb6f6e042bc1062e3dcb1d6e
Michael Odikanwa <odikanwamichael@gmail.com>
2024-02-11 12:19:04 +0100

    Tuples introduction completed

---
+++ data_structures_tuples.py
@@ -0,0 +1,8 @@
+# Tuples - A DS for immutable ordered sequences of elements. Used for grouping elements that always go together
+
+coordinates = 3.454566, 6.433355
+print(coordinates)
+
+# Unpacking tuples
+longitude, latitude = coordinates
+print(longitude, latitude)
