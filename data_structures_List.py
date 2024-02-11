1c9d64b036600a429bb99c6add94ec82f0039d05
Michael Odikanwa <odikanwamichael@gmail.com>
2024-02-11 11:40:24 +0100

    list DS completed

--- data_structures_List.py
+++ data_structures_List.py
@@ -20,3 +20,21 @@ print(week_days[2:])
 # Operations - Memberships & Identity
 print("Wed" in week_days)
 print("Flat" not in week_days)
+
+# Mutability
+week_days[3] = "Thur"
+print(week_days)
+
+# List Methods - len(), max(), min(), sorted(), join(), append
+
+numbers = [0, 2, 8, 6 , 10, 4]
+# SORTED - Sort by default - ascending
+print(sorted(numbers))
+# SORTED - Sort in reverse
+print(sorted(numbers, reverse=True))
+# JOIN - Join the days of the week
+joined_weekdays = "\n".join(week_days) #Join by new line
+print(joined_weekdays)
+# APPEND - Add element to the end of the list
+numbers.append(12)
+print(numbers)
